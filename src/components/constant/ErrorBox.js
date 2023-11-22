cc296903e0b7679ff89c3e25fef4bc839e80e4e7
branow <bodnarorestdraw@gmail.com>
2023-11-22 12:34:34 +0200

    Add authentication check in PrivateUserPage, modify ErrorBox

--- src/components/constant/ErrorBox.js
+++ src/components/constant/ErrorBox.js
@@ -1,6 +1,6 @@
 const ErrorBox = ({ title, message }) => {
   return (
-    <div className="flex flex-col items-center justify-center p-4 mt-[10px] mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300 ">
+    <div className="flex flex-col items-center justify-center p-4 text-sm text-yellow-800 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300 ">
       <div className="text-[16px] font-bold">
         <span>{title}</span>
       </div>
