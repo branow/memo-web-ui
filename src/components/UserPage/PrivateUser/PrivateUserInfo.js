454dea589f1d57e1dbd5adc7ebd008e4450d3b8c
branow <bodnarorestdraw@gmail.com>
2023-11-22 22:45:01 +0200

    modify css of PrivateUserInfo and UserSetting

--- src/components/UserPage/PrivateUser/PrivateUserInfo.js
+++ src/components/UserPage/PrivateUser/PrivateUserInfo.js
@@ -62,64 +62,62 @@ const PrivateUserInfo = () => {
     });
   };
 
-  // console.log("Private User Info: username: " + username + ", email: " + email + ", description: " + description);
-
   return (
-    <div className="relative w-screen h-screen bg-form-background-grey text-white border-l-2 border-left-solid border-main-green flex flex-row">
-      <div>
-        <div className="text-3xl font-semibold ml-[12vw] mt-[5vh] w-fit">
-          <span>Profile Info</span>
-          <RxAvatar className="m-auto mt-[20px]" color="white" size="70px" />
-        </div>
-        {useGet.state.error && (
-          <ErrorBox
-            title={"Authentification error"}
-            message={useGet.state.error}
-          />
-        )}
-        {useSave.state.error && (
-          <ErrorBox title={"Saving error"} message={useSave.state.error} />
-        )}
-        {(useGet.state.pending || useSave.state.pending) && (
-          <LoadingScreen />
-        )}
-
-        {useGet.userState.user && (
-          <>
-            <div className="ml-[7vw] mt-[5vh] w-[20vw]">
-              <div className="my-[8vh]">
-              <UserNameInputField
-                      onChangeAction={(e) =>
-                        setUsername(e.target.value)
-                      }
-                      value={useGet.userState.user.username}
-                    />
-              </div>
-              <div className="my-[8vh]">
-
-                    <EmailInputField
-                      onChangeAction={(e) => setEmail(e.target.value)}
-                      value={useGet.userState.user.email}
-                    />
-              </div>
-            </div>
-            <div className="ml-[7vw]">
-              <SubmitButton onClickAction={handleSumbit} actionName={"Save"} />
-            </div>
-          </>
-        )}
+    <div className="relative w-screen h-screen bg-form-background-grey text-white border-l-2 border-left-solid border-main-green flex flex-col items-center">
+      <div className="mt-[3vh] text-3xl font-semibold flex flex-col items-center">
+        <div className="m-[20px]">Profile Info</div>
+        <RxAvatar className="m-[10px]" color="white" size="70px" />
       </div>
-      <div className="text-xl font-medium ml-[15vw] mt-[20vh] flex flex-col w-[25vw]">
-        <span>Description</span>
-        {useGet.userState.user && (
-          <textarea
-            className="mt-[20px] bg-form-background-grey text-white border-[1px] border-left-solid border-gray-300 rounded-lg p-5"
-            cols="30"
-            rows="10"
-            defaultValue={useGet.userState.user.description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        )}
+      <div className="flex items-center justify-around py-[4vh] px-[2vw] w-[85%]">
+        <div className="flex flex-col items-center px-[2wh]">
+          {useGet.state.error && (
+            <ErrorBox
+              title={"Authentification error"}
+              message={useGet.state.error}
+            />
+          )}
+          {useSave.state.error && (
+            <ErrorBox title={"Saving error"} message={useSave.state.error} />
+          )}
+          {(useGet.state.pending || useSave.state.pending) && <LoadingScreen />}
+
+          {useGet.userState.user && (
+            <>
+              <div className="">
+                <div className="my-[5vh]">
+                  <UserNameInputField
+                    onChangeAction={(e) => setUsername(e.target.value)}
+                    value={useGet.userState.user.username}
+                  />
+                </div>
+                <div className="my-[5vh]">
+                  <EmailInputField
+                    onChangeAction={(e) => setEmail(e.target.value)}
+                    value={useGet.userState.user.email}
+                  />
+                </div>
+              </div>
+              <div className="my-[1vw]">
+                <SubmitButton
+                  onClickAction={handleSumbit}
+                  actionName={"Save"}
+                />
+              </div>
+            </>
+          )}
+        </div>
+        <div className="text-xl font-medium flex flex-col items-center px-[2vw]">
+          <div>Description</div>
+          {useGet.userState.user && (
+            <textarea
+              className="text-sm mt-[20px] bg-form-background-grey text-white border-[1px] border-left-solid border-gray-300 rounded-lg p-5"
+              cols="40"
+              rows="10"
+              defaultValue={useGet.userState.user.description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
--- src/components/UserPage/PrivateUser/UserSettings.js
+++ src/components/UserPage/PrivateUser/UserSettings.js
@@ -22,59 +22,78 @@ const ProfileSettings = () => {
       confirmPassword: confirmPassword,
     };
     useChange.state.run(dto);
-  }, [useChange.state, currentPassword, newPassword, confirmPassword]);
+  }, [useChange, currentPassword, newPassword, confirmPassword]);
 
   const deleteCurrentUser = useCallback(() => {
-    if (window.confirm("After deletion, the account cannot be restored. Do you really want to delete your account?")) {
+    if (
+      window.confirm(
+        "After deletion, the account cannot be restored. Do you really want to delete your account?"
+      )
+    ) {
       useDelete.state.run();
     }
-  }, [useDelete.state]);
+  }, [useDelete]);
 
   return (
     <div className="relative w-screen h-screen bg-form-background-grey text-white border-l-2 border-left-solid border-main-green">
       <div className="mt-[10vh] flex flex-wrap justify-around">
-      <div className="flex flex-col items-center">
-        <div className="w-fit">
-          <div className="text-xl font-medium m-auto">Password</div>
-        </div>
-        {useChange.state.error && (
-          <ErrorBox title="Changing password error" message={useChange.state.error} />
-        )}
-        {useChange.state.pending && <LoadingScreen />}
-        <div className="">
-          <div className="my-[5vh]">
-                <PasswordInputField
-                  onChangeAction={useCallback((e) => setCurrentPassword(e.target.value), [])}
-                />
+        <div className="flex flex-col items-center">
+          <div className="w-fit">
+            <div className="text-xl font-medium my-[20px]">Password</div>
           </div>
-          <div className="my-[5vh]">
-                <PasswordInputField
-                  onChangeAction={useCallback((e) => setNewPassword(e.target.value), [])}
-                />
+          {useChange.state.error && (
+            <ErrorBox
+              title="Changing password error"
+              message={useChange.state.error}
+            />
+          )}
+          {useChange.state.pending && <LoadingScreen />}
+          <div className="">
+            <div className="my-[4vh]">
+              <PasswordInputField
+                label="Current Password"
+                onChangeAction={useCallback(
+                  (e) => setCurrentPassword(e.target.value),
+                  []
+                )}
+              />
+            </div>
+            <div className="my-[4vh]">
+              <PasswordInputField
+                label="New Password"
+                onChangeAction={useCallback(
+                  (e) => setNewPassword(e.target.value),
+                  []
+                )}
+              />
+            </div>
+            <div className="my-[4vh]">
+              <PasswordInputField
+                label="Confirm New Password"
+                onChangeAction={useCallback(
+                  (e) => setConfirmPassword(e.target.value),
+                  []
+                )}
+              />
+            </div>
           </div>
-          <div className="my-[5vh]">
-                <PasswordInputField
-                  onChangeAction={useCallback((e) => setConfirmPassword(e.target.value), [])}
-                />
+          <div className="">
+            <SubmitButton actionName={"Save"} onClickAction={handleSumbit} />
           </div>
         </div>
-        <div className="">
-          <SubmitButton actionName={"Save"} onClickAction={handleSumbit} />
+        <div className="w-[25vw] flex flex-col items-center">
+          <div className="text-xl font-medium my-[20px]">Advanced</div>
+          {useDelete.state.error && (
+            <ErrorBox title="Deleting error" message={useDelete.state.error} />
+          )}
+          {useDelete.state.pending && <LoadingScreen />}
+          <div className="">
+            <DeleteButton
+              actionName={"Delete account"}
+              onClickAction={deleteCurrentUser}
+            />
+          </div>
         </div>
-      </div>
-      <div className="w-[25vw] flex flex-col items-center">
-        <div className="text-xl font-medium">Advanced</div>
-        {useDelete.state.error && (
-          <ErrorBox title="Deleting error" message={useDelete.state.error} />
-        )}
-        {useDelete.state.pending && <LoadingScreen />}
-        <div className="mt-[30px]">
-          <DeleteButton
-            actionName={"Delete account"}
-            onClickAction={deleteCurrentUser}
-          />
-        </div>
-      </div>
       </div>
     </div>
   );
