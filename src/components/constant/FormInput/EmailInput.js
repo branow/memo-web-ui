f08a6b8db722a5341a0eb7b9d70453d48f827322
branow <bodnarorestdraw@gmail.com>
2023-11-22 08:42:00 +0200

    add using memo() in inputs and rename params of ErrorBox

--- src/components/constant/FormInput/EmailInput.js
+++ src/components/constant/FormInput/EmailInput.js
@@ -1,4 +1,6 @@
-const EmailInput = ({ onChangeAction }) => {
+import { memo } from "react";
+
+const EmailInput = memo(({ onChangeAction, value }) => {
   return (
     <input
       className="peer w-[100%] h-[100%] bg-transparent text-[1em] text-white font-semibold pl-[5px] pr-[35px] border-none outline-none"
@@ -7,8 +9,9 @@ const EmailInput = ({ onChangeAction }) => {
       title="Must be in specified format: someemail@mail.ma"
       onChange={onChangeAction}
       placeholder=""
+      defaultValue={value}
     />
   );
-};
+});
 
 export default EmailInput;
