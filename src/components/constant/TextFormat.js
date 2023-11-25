7d41aa8305716d5056887f4f521cf616d1989a33
branow <bodnarorestdraw@gmail.com>
2023-11-25 16:03:21 +0200

    add TextFormat component

---
+++ src/components/constant/TextFormat.js
@@ -0,0 +1,53 @@
+const TextFormat = ({ textnode, text }) => {
+  let start = textnode.start;
+  let end = 0;
+  let inner = (
+    <>
+      {textnode.children.map((e) => {
+        end = e.start;
+        var t1 = text.substring(start, end);
+        var child = (
+          <>
+            {t1}
+            <TextFormat textnode={e} text={text} />
+          </>
+        );
+        start = e.end;
+        return child;
+      })}
+      {text.substring(start, textnode.end)}
+    </>
+  );
+
+  return (
+    <>
+      <TextFormatTeg
+        format={textnode.format.size}
+        inner={<TextFormatTeg format={textnode.format.color} inner={inner} />}
+      />
+    </>
+  );
+};
+
+const TextFormatTeg = ({ format, inner }) => {
+  return (
+    <>
+      {!format && <>{inner}</>}
+      {format === "bold" && <b>{inner}</b>}
+      {format === "italic" && <i>{inner}</i>}
+      {format === "underline" && <u>{inner}</u>}
+
+      {format === "small" && <span className="text-sm">{inner}</span>}
+      {format === "large" && <span className="text-xl">{inner}</span>}
+
+      {format === "red" && <span className="text-red-500">{inner}</span>}
+      {format === "blue" && <span className="text-blue-500">{inner}</span>}
+      {format === "white" && <span className="text-gray-100">{inner}</span>}
+      {format === "black" && <span className="text-gray-900">{inner}</span>}
+      {format === "green" && <span className="text-green-500">{inner}</span>}
+      {format === "yellow" && <span className="text-yellow-500">{inner}</span>}
+    </>
+  );
+};
+
+export default TextFormat;
