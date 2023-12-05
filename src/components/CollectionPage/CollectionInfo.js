4a591263e978ac2ff7f12a3fab2510ebd966c8d8
branow <bodnarorestdraw@gmail.com>
2023-12-05 11:33:49 +0200

    add CollectionEditForm component

--- src/components/CollectionPage/CollectionInfo.js
+++ src/components/CollectionPage/CollectionInfo.js
@@ -2,48 +2,21 @@ import ChangeCircleButton from "../constant/Buttons/ChangeCircleButton";
 import ScoreWrapper from "../UserPage/PublicUser/ScoreWrapper";
 import Avatar from "../constant/Icons/Avatar";
 import DownloadCircleButton from "../constant/Buttons/DownloadCircleButton";
-import { useSaveCollection } from "../../hooks/request/collection";
 import { useContext, useState } from "react";
 import WindowWrapper from "../constant/WindowWrapper";
-import EditForm from "../constant/Forms/EditForm";
-import ErrorBox from "../constant/ErrorBox";
-import LoadingAnimation from "../constant/LoadingAnimation";
 import { CollectionContext } from "./CollectionPage";
+import CollectionEditForm from "./CollectionEditForm";
 
 const CollectionInfo = () => {
   const { collectionState, isOwner, isAuthenticated } = useContext(CollectionContext);
-  const defaultCollection = collectionState.collection;
-
-  const useSave = useSaveCollection((savedCollection) => {
-    collectionState.setCollection((pr) => {
-      pr.collectionName = savedCollection.collectionName;
-      return Object.assign({}, pr);
-    });
-  });
-  const [isEditName, setIsEditName] = useState(false);
-
-  const onSaveNewName = (newName) => {
-    if (defaultCollection.collectionName !== newName) {
-      const collection = {
-        collectionId: defaultCollection.collectionId,
-        collectionName: newName,
-      };
-      const moduleId = defaultCollection.module.moduleId;
-      useSave.state.run({ moduleId, collection });
-    }
-  };
+  const [isEdit, setIsEdit] = useState(false);
+  const collection = collectionState.collection;
 
   return (
     <>
-      {isEditName && (
-        <WindowWrapper close={() => setIsEditName(false)}>
-          <EditForm
-            title="Collection Name"
-            defaultValue={defaultCollection.collectionName}
-            actionName="Save"
-            close={() => setIsEditName(false)}
-            onSubmit={onSaveNewName}
-          />
+      {isEdit && (
+        <WindowWrapper close={() => setIsEdit(false)}>
+          <CollectionEditForm collectionState={collectionState} close={() => setIsEdit(false)} />
         </WindowWrapper>
       )}
 
@@ -51,46 +24,36 @@ const CollectionInfo = () => {
         <div className="w-[45vw] flex flex-col">
           <div>
             <span className="text-2xl text-main-green">
-              {defaultCollection.module.moduleName}
+              {collection.module.moduleName}
             </span>
           </div>
           <div className="text-4xl mt-[3vh]">
-            {useSave.state.pending && (
-              <LoadingAnimation message="Renaming..." />
-            )}
-            {useSave.state.error && (
-              <ErrorBox
-                title="Collection Rename Error"
-                message={useSave.state.error}
-                close={useSave.state.cleanError}
-              />
-            )}
             <span className="font-normal">
-              {defaultCollection.collectionName}
+              {collection.collectionName}
             </span>
             {isOwner && (
               <span className="ml-[0.5vw]">
                 <ChangeCircleButton
                   size={"25px"}
-                  onClickAction={() => setIsEditName(true)}
+                  onClickAction={() => setIsEdit(true)}
                 />
               </span>
             )}
 
             <span className="pr-[1vw] font-semibold text-main-green float-right border-r-[4px] border-solid border-white">
-              {defaultCollection.flashcardIds.length}
+              {collection.flashcardIds.length}
             </span>
           </div>
         </div>
         <div className="h-[5vh] mt-[2vh] ml-[5vw] z-10">
-          <ScoreWrapper scores={defaultCollection.scores} />
+          <ScoreWrapper scores={collection.scores} />
         </div>
         <div className="absolute top-0 right-0 mt-[2vh] mr-[2vw]">
           <Avatar size={"75px"} />
           <div className="w-fit m-auto text-lg">
-            {defaultCollection.owner.username}
+            {collection.owner.username}
           </div>
-          {isAuthenticated && !isOwner && (
+          {collection && !isOwner && (
             <div className="w-fit m-auto">
               <DownloadCircleButton size={"40px"} />
             </div>
