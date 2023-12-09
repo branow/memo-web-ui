5f752537e7f1b23578707f9510d5b680e94d379b
branow <bodnarorestdraw@gmail.com>
2023-12-09 16:03:50 +0200

    connect ModuleCollection and CollectionInfo with ImportCollection

--- src/components/ModulePage/ModuleCollection.js
+++ src/components/ModulePage/ModuleCollection.js
@@ -4,10 +4,12 @@ import StudyTypeDescription from "../UserPage/PublicUser/Module/StudyTypeDescrip
 import DownloadCircleButton from "../constant/Buttons/DownloadCircleButton";
 import { Link } from "react-router-dom";
 import { useDeleteCollection } from "../../hooks/request/collection";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ModuleContext } from "./ModulePage";
 import LoadingAnimation from "../constant/LoadingAnimation";
 import ErrorBox from "../constant/ErrorBox";
+import WindowWrapper from "../constant/WindowWrapper";
+import ImportCollection from "../ImportWindow/ImportCollection";
 
 const ModuleCollection = ({ collection }) => {
   const { moduleState, isAuthenticated, isOwner } = useContext(ModuleContext);
@@ -19,6 +21,7 @@ const ModuleCollection = ({ collection }) => {
       return Object.assign({}, pr);
     });
   });
+  const [isImport, setIsImport] = useState(false);
 
   const handleOnDelete = () => {
     if (
@@ -32,49 +35,61 @@ const ModuleCollection = ({ collection }) => {
   };
 
   return (
-    <div
-      className="[&:hover>div.hidden]:block relative w-[50vw] h-fit  mx-auto my-[2vh] bg-charcoal mt-[2vh] rounded-t-lg border-b-[4px] 
+    <>
+      {isImport && (
+        <WindowWrapper close={() => setIsImport(false)}>
+          <ImportCollection collectionId={collection.collectionId} close={() => setIsImport(false)} />
+        </WindowWrapper>
+      )}
+      <div
+        className="[&:hover>div.hidden]:block relative w-[50vw] h-fit  mx-auto my-[2vh] bg-charcoal mt-[2vh] rounded-t-lg border-b-[4px] 
     border-charcoal hover:border-solid hover:border-regent-grey "
-    >
-      {useDelete.state.pending && <LoadingAnimation message="Deleting..." />}
-      {useDelete.state.error && (
-        <ErrorBox
-          title="Collection Delete Error"
-          message={useDelete.state.error}
-          close={useDelete.state.cleanError}
-        />
-      )}
-      <div className="hidden relative float-right p-[0.5vh]">
-        {isOwner && (
-          <DeleteCircleButton size={"25px"} onClickAction={handleOnDelete} />
-        )}
-        {isAuthenticated && !isOwner && <DownloadCircleButton size={"25px"} />}
-      </div>
-      <div className="flex flex-row px-[3vw] py-[3vh]">
-        <div className="w-[22vw]">
-          <Link to={"/collection/" + collection.collectionId}>
-            <span className="text-2xl font-light cursor-pointer hover:underline hover:decoration-2">
-              {collection.collectionName}
-            </span>
-          </Link>
-          <span className="float-right text-3xl text-main-green font-semibold">
-            {collection.size}
-          </span>
-        </div>
-        <div className="w-[12vw] h-[9vh] mt-[-2vh] ml-[6vw]">
-          <ScoreWrapper scores={collection.scores} size="SMALLER" />
-        </div>
-      </div>
-      {isOwner && (
-        <div className="border-t-[4px] border-solid border-tealish-blue">
-          <StudyTypeDescription
-            collections={[collection.collectionId]}
-            buttonsClassname={"text-dim-grey"}
-            collectionDescription={true}
+      >
+        {useDelete.state.pending && <LoadingAnimation message="Deleting..." />}
+        {useDelete.state.error && (
+          <ErrorBox
+            title="Collection Delete Error"
+            message={useDelete.state.error}
+            close={useDelete.state.cleanError}
           />
+        )}
+        <div className="hidden relative float-right p-[0.5vh]">
+          {isOwner && (
+            <DeleteCircleButton size={"25px"} onClickAction={handleOnDelete} />
+          )}
+          {isAuthenticated && !isOwner && (
+            <DownloadCircleButton
+              size={"25px"}
+              onClickAction={() => setIsImport(true)}
+            />
+          )}
         </div>
-      )}
-    </div>
+        <div className="flex flex-row px-[3vw] py-[3vh]">
+          <div className="w-[22vw]">
+            <Link to={"/collection/" + collection.collectionId}>
+              <span className="text-2xl font-light cursor-pointer hover:underline hover:decoration-2">
+                {collection.collectionName}
+              </span>
+            </Link>
+            <span className="float-right text-3xl text-main-green font-semibold">
+              {collection.size}
+            </span>
+          </div>
+          <div className="w-[12vw] h-[9vh] mt-[-2vh] ml-[6vw]">
+            <ScoreWrapper scores={collection.scores} size="SMALLER" />
+          </div>
+        </div>
+        {isOwner && (
+          <div className="border-t-[4px] border-solid border-tealish-blue">
+            <StudyTypeDescription
+              collections={[collection.collectionId]}
+              buttonsClassname={"text-dim-grey"}
+              collectionDescription={true}
+            />
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
